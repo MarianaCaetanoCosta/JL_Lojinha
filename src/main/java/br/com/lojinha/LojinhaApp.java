1b7933fc12b79862a3546871d18189e625313a33
Mariana Caetano <mariana.cae.costa@gmail.com>
2024-07-24 17:05:21 -0300

    #3 - Classe e Atributos
    1. Criado packege Pojo
    2. Criado Classse ProdutoPojo e implementado váriaveis publicas
    3. Na LojinhaApp método main foi importado a classe ProdutoPojo, instanciado o objeto, passado valor aos atributos do produto e impresso no console

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -1,16 +1,23 @@
 package br.com.lojinha;
 
+import br.com.lojinha.pojo.ProdutoPojo;
+
 public class LojinhaApp {
     public static void main(String[] args) {
+        //Importar a classe pojo > Produto e instanciar o objeto
+        ProdutoPojo meuProdutoPojo = new ProdutoPojo();
 
-        String joia = "100g de ouro 50k";
-        int minhaIdade = 37;
-        double meuSalario = 5.400;
-        boolean minhaResposta = false;
+        //Passar valor aos atributos da classe
+        meuProdutoPojo.nome = "Playstation 5";
+        meuProdutoPojo.valor = 4599.99;
+        meuProdutoPojo.marca = "Sony";
+        meuProdutoPojo.tamanho = "Medio";
+        meuProdutoPojo.itensInclusos = "2 Controle - 3 Jogos";
 
-        System.out.println("Joia: " + joia);
-        System.out.println("Idade: " + minhaIdade);
-        System.out.println("Salário: " + meuSalario);
-        System.out.println("Resposta: " + minhaResposta);
+        System.out.println("Nome: " + meuProdutoPojo.nome);
+        System.out.println("Valor: " + meuProdutoPojo.valor);
+        System.out.println("Marca: " + meuProdutoPojo.marca);
+        System.out.println("Tamanho: " + meuProdutoPojo.tamanho);
+        System.out.println("Itens inclusos: " + meuProdutoPojo.itensInclusos);
     }
 }
