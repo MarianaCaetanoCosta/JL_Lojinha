0103d5d4ddd45f1c9a9bf586b5b15ae26c816c63
Mariana Caetano <mariana.cae.costa@gmail.com>
2024-07-24 16:47:56 -0300

    #2 - Uso de variáveis
    1. uso da variáveis String, int, double e boolean.
    2. impressão das variáveis com o texto concatenado.

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -2,6 +2,15 @@ package br.com.lojinha;
 
 public class LojinhaApp {
     public static void main(String[] args) {
-        System.out.println("Lojinha");
+
+        String joia = "100g de ouro 50k";
+        int minhaIdade = 37;
+        double meuSalario = 5.400;
+        boolean minhaResposta = false;
+
+        System.out.println("Joia: " + joia);
+        System.out.println("Idade: " + minhaIdade);
+        System.out.println("Salário: " + meuSalario);
+        System.out.println("Resposta: " + minhaResposta);
     }
 }
